bdce3627ddb056a7bfff317c2efcc5cba0c025a6
sorin <iatcosorin797@gmail.com>
2022-09-30 17:00:11 +0300

    Add csgo

--- csgo.kt
+++ csgo.kt
@@ -107,9 +107,8 @@ private class Shoot {
     init {
         shot()
     }
-}
+open class Weapons {
 
-private class Weapons {
     var ctPistol = mapOf("USP-S" to 1, "Dual Berettas" to 2, "P250" to 3, "Five-SeveN" to 4, "Desert Eagle" to 5)
 
     var ctHeavy = mapOf("Nova" to 1, "XM1014" to 2, "MAG-7" to 3, "M249" to 4, "Negev" to 5)
@@ -119,19 +118,40 @@ private class Weapons {
     var ctRifle = mapOf("FAMAS" to 1, "M4A1-S" to 2, "SSG 08" to 3, "AUG" to 4, "AWP" to 5, "SCAR-20" to 6)
 
     var ctGrenade = mapOf("Incendiary Grenade" to 1, "Decoy Grenade" to 2, "Flashbang" to 3, "High Explosive Grenade" to 4,
-                            "Smoke Grenade" to 5)
+        "Smoke Grenade" to 5)
 
     var ctEquipment = mapOf("Kevlar Vest" to 1, "Kevlar + Helmet" to 2, "Zeus x27" to 3, "Defuse Kit" to 4)
-
-    fun stats() {
-        // need to get the stats for the specific gun in the directory
-        // damage and armor penetration
-        // fire-rate and others
-    }
 
     init {
         println("Pistols are: $ctPistol")
     }
+}
+
+private class Stats: Weapons() {
+
+    var ammo = ""
+
+    fun ammo() {
+        println("Input your weapon: ")
+        for (x in ctPistol.values) {
+            when (x) {
+                1 -> {ammo = "12/24"
+                    println(ammo)}
+                2 -> {ammo = "30/120"
+                    println(ammo)}
+                3 -> {ammo = "13/26"
+                    println(ammo)}
+                4 -> {ammo = "20/100"
+                    println(ammo)}
+                5 -> {ammo = "7/35"
+                    println(ammo)}
+            }
+        }
+    }
+        // need to get the stats for the specific gun in the directory
+        // damage and armor penetration
+        // fire-rate and others
+}
 }
 
 private class Bomb {
@@ -175,28 +195,28 @@ private class Radar {
 private class Buy {
     // Buy menu
     fun read() {
-        var categoryChoise = ""
+        var categoryChoice = ""
         println("What type of weapon do you want to buy? ")
         val categoryInput = readLine()!!
         when (categoryInput.toInt()) {
-            1 -> categoryChoise = "pistol"
-            2 -> categoryChoise = "heavy"
-            3 -> categoryChoise = "smg"
-            4 -> categoryChoise = "rifle"
-            5 -> categoryChoise = "grenade"
-            6 -> categoryChoise = "equipment"
+            1 -> categoryChoice = "pistol"
+            2 -> categoryChoice = "heavy"
+            3 -> categoryChoice = "smg"
+            4 -> categoryChoice = "rifle"
+            5 -> categoryChoice = "grenade"
+            6 -> categoryChoice = "equipment"
             else -> "Not in categories!"
         }
         println("What model do you want to buy? ")
         val typeInput = readLine()!!
         when (typeInput.toInt()) {
             // the specific model of a category
-            1 -> println("You bought a: $categoryChoise")
-            2 -> println("You bought a: $categoryChoise")
-            3 -> println("You bought a: $categoryChoise")
-            4 -> println("You bought a: $categoryChoise")
-            5 -> println("You bought a: $categoryChoise")
-            6 -> println("You bought a: $categoryChoise")
+            1 -> println("You bought a: $categoryChoice")
+            2 -> println("You bought a: $categoryChoice")
+            3 -> println("You bought a: $categoryChoice")
+            4 -> println("You bought a: $categoryChoice")
+            5 -> println("You bought a: $categoryChoice")
+            6 -> println("You bought a: $categoryChoice")
             else -> "Not in the models!"
         }
     }
@@ -290,7 +310,7 @@ fun main() {
 
     val obj7 = Kill()
 
-    val obj8 = Weapons()
+    val obj8 = Stats()
 
     val obj9 = Bomb()
 
