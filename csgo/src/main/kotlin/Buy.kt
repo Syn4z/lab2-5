f6630fc8dfd72d04d7d7468eb5fc8bd8a472fc48
sorin <iatcosorin797@gmail.com>
2022-10-17 19:02:02 +0300

    Update scenario

--- csgo/src/main/kotlin/Buy.kt
+++ csgo/src/main/kotlin/Buy.kt
@@ -3,7 +3,7 @@ abstract class Buy: Weapons() {
     var typeInput = 0
     var categoryChoice = mapOf("" to 0)
     var weapons = listOf("")
-    var i = 10
+    private var i = 10
     private var ammo = mapOf("12/24" to 1, "30/110" to 2, "13/26" to 3, "20/100" to 4, "7/35" to 5,
         "8/32" to 6, "7/32" to 7, "5/32" to 8, "100/200" to 9, "150/300" to 10, "30/130" to 11, "30/120" to 12,
         "25/100" to 13, "50/100" to 14, "64/120" to 15, "25/90" to 16, "20/80" to 17, "10/90" to 18, "30/90" to 19,
--- csgo/src/main/kotlin/Main.kt
+++ csgo/src/main/kotlin/Main.kt
@@ -1,6 +1,14 @@
 fun main() {
     val obj1 = Kill()
     obj1.isKill()
-    println(obj1.counterTerrorists)
-    println(obj1.terrorists)
+    if (obj1.counterTerrorists == 0) {
+        println("\n\tTerrorists Win!")
+    }
+    else {
+        if (obj1.terrorists == 0) {
+            println("\n\tCounterTerrorists Win!")
+        }
+    }
+    println("\nRemaining CounterTerrorists: ${obj1.counterTerrorists}")
+    println("Remaining Terrorists: ${obj1.terrorists}")
 }
